dbcd9fd718e7db8a728fd0f99d628b337ab0934a
rouxpz <roopa.vasudevan@gmail.com>
2019-06-26 18:10:22 +0200

    updating innerHTML on affect change

--- js/bot.js
+++ js/bot.js
@@ -1,5 +1,6 @@
 let posCats = ['nn', 'vb', 'jj', 'prp', 'rb', 'ex'];
-let groups = [[], [], [], [], [], []]
+let groups = [[], [], [], [], [], []];
+let constructions = [['nn', 'vb', 'jj'], ['prp', 'vb', 'jj']];
 
 function loadData() {
   request = new XMLHttpRequest();
@@ -31,7 +32,7 @@ function loadData() {
 
       // console.log(words.length);
       // console.log(words[0]);
-      console.log(groups);
+      // console.log(groups);
     } else {
       // We reached our target server, but it returned an error
       console.log("error");
@@ -42,8 +43,44 @@ function loadData() {
 }
 
 function changeAffect() {
+  document.getElementById("pastStatements").innerHTML += '<br>' + document.getElementById("mainStatement").innerHTML;
+
+  var currentText = '';
+  var toAdd;
+  var cs = Math.floor(Math.random() * constructions.length);
   var affects = document.getElementById("affects");
-  document.getElementById("mainStatement").innerHTML = affects.value;
+
+  for (var i = 0; i < constructions[cs].length; i++) {
+    if (constructions[cs][i] === 'prp') {
+      //select proper noun randomly
+      toAdd = Math.floor(Math.random() * groups[3].length);
+      currentText += groups[3][toAdd].text + ' ';
+      console.log("proper noun");
+    } else {
+      //match affect
+      var s = posCats.indexOf(constructions[cs][i]);
+      var tempArray = [];
+      // console.log(groups[s]);
+      for (var j = 0; j < groups[s].length; j++) {
+        if (groups[s][j].affects.indexOf(affects.value) != -1) {
+          //temporary array to select from
+          tempArray.push(groups[s][j].text);
+          // console.log(posCats[s]);
+          // console.log(groups[s][j].affects);
+        }
+      }
+      // console.log(tempArray);
+      if (tempArray.length > 0) {
+        //randomly select word
+        toAdd = Math.floor(Math.random() * tempArray.length);
+        currentText += tempArray[toAdd] + ' ';
+      } else {
+        //randomly select from full array of POS
+      }
+    }
+  }
+
+  document.getElementById("mainStatement").innerHTML = currentText;
   // console.log(affects.value);
 }
 
